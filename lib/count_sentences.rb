b4e5d9141de5d82ef27bfbcbef05c78b659ac3c2
Lester Young <lyoung070789@gmail.com>
2020-02-18 00:18:47 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count = 0
+    
+    string_array = self.split(/[.!?]/)
+    string_array.each do |i|
+      if i == ""
+        count == count
+      else 
+      count+=1
+      end 
+    end 
+    return count
   end
 end
