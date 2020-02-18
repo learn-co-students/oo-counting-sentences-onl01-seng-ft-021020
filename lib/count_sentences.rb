aacc9bd35a298a38c3563fd8403dff58fb75fb98
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-18 23:20:32 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return (self[-1] == ".")
   end
 
   def question?
-
+    return (self[-1] == "?")
   end
 
   def exclamation?
-
+    return (self[-1] == "!")
   end
 
   def count_sentences
-
+    counter = 0
+    
+    sentences = self.split(/[.!?]/)
+    sentences.each do |s|
+      if(s.length >= 1)
+        counter += 1
+      end
+    end
+   
+    
+    counter
+    
   end
 end
